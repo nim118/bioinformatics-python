822b862aea0a5a8a9e0732e9be4bcc375fe11148
Nimesh Ganatra <nimesh@topsinfosolutions.com>
2019-02-05 17:05:41 +0530

    rabbit and recurrenece relation

--- rabbits_and_recurrence_relations.py
+++ rabbits_and_recurrence_relations.py
@@ -1,5 +1,21 @@
-n = 5 
+n = 35
 #n is number of months
+#fn = fn-1 + fn-2*k
+#Positive integers n≤40 and k≤5.
 
-k = 3
-#k is number of rabiits pairs reproduced by every pair
+k = 4
+#k is number of rabiits pairs reproduced by every pair 
+
+serise = [1, 1]
+count = 1
+
+while count < n-1:
+	serise.append(serise[count] + serise[count-1] * k)
+	count = count + 1
+
+
+print serise
+
+print serise[len(serise) - 1]
+
+print len(serise)
