0ed1982482812b09510d5da9610881238ed12af7
Nimesh Ganatra <g.nimesh118@gmail.com>
2019-06-02 17:01:08 +0530

    moved files to its dir

---
+++ bioinformatics-stronghold/rabbits_and_recurrence_relations.py
@@ -0,0 +1,22 @@
+#Problem Url : http://rosalind.info/problems/fib/
+n = 20
+#n is number of months
+#fn = fn-1 + fn-2*k
+#Positive integers n<=40 and k<=5.
+
+k = 1
+#k is number of rabiits pairs reproduced by every pair 
+
+serise = [1, 1]
+count = 1
+
+while count < n-1:
+	serise.append(serise[count] + serise[count-1] * k)
+	count = count + 1
+
+
+#print serise
+	
+print serise[len(serise) - 1]
+
+#print len(serise)
